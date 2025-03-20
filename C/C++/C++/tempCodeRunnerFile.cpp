b93acc074e45b332c520edba2d9eefc81bb9c446
lee <wjl100735@gmail.com>
2025-03-20 13:40:31 +0900

    pop C, C++구현

--- C/C++/C++/tempCodeRunnerFile.cpp
+++ C/C++/C++/tempCodeRunnerFile.cpp
@@ -1,12 +1,2 @@
-#include <iostream>
-using namespace std;
-#include "Playfair.h"
 
-int main() {
-	Playfair pfair("assasinator", "q/z");
-	//asintior
-	pfair.makeTable();
-	pfair.showTable();
-	
-	cout<<"최종: "+pfair.makeEncryption("becsrefull");
 }
